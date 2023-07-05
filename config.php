3f6b853f0f07442edb019521a22d1a2f8a18dd7d
Sage <86696065+GoSwimmy@users.noreply.github.com>
2023-07-05 15:41:29 -0500

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ return [
     'baseUrl' => '',
     'github' => 'https://github.com/GoSwimmy',
     'twitter' => '#',
-    'discord' => 'https://discord.com/users/200692983471931392',
+    'discord' => 'https://discord.com/users/952784093647601704',
     'collections' => [],
 ];
